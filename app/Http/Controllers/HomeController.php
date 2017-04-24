6302f2ce089fd4542ab2f67bc4d0664bbbbc07d2
Matt Valdez <mattvaldez01@gmail.com>
2017-04-24 16:04:52 -0500

    made HomeController that is used on routes examples

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class HomeController extends Controller
+{
+    public function showWelcome()
+    {
+        return view('welcome');
+    }
+
+    public function sayHello($name)
+	{
+    	$data = ['name' => $name];
+    	return view('my-first-view', $data);
+    }
+
+    public function uppercase($word) 
+    {
+    	$data = ['low' => $word, 'caps' => strtoupper("$word")]; 
+    	return view('uppercase', $data);
+	}
+
+	public function increment($num) {
+    	$data = ['num' => ++$num];
+    	return view('increment', $data);
+	}
+}
