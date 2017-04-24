813ce37362c6fcf23a6b925b256471ad87081cee
Matt Valdez <mattvaldez01@gmail.com>
2017-04-24 16:58:34 -0500

    refactored code to use controller actions

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -23,22 +23,7 @@ Route::get('/uppercase/{word?}', 'HomeController@uppercase');
 Route::get('/increment/{num?}', 'HomeController@increment');
 
 
-Route::get('/add/{num1?}/{num2?}', function ($num1, $num2) {
-	if (is_numeric($num1) && is_numeric($num2)) {
-	    return $num1+$num2;
-	} else {
-		return "Both inputs must be numeric.";
-	}
-});
+Route::get('/add/{num1?}/{num2?}', 'ExampleController@add');
 
 
-Route::get('/rolldice/{guess?}', function ($guess = null) {
-	if (!is_numeric($guess)) {
-		return $guess . " is not numeric. Guess Again.";
-	} elseif ($guess < 1 || $guess > 6) {
-		return $guess . " is not between 1 and 6. Guess Again.";
-	} else {
-		$data = ['diceNum' => rand(1, 6), 'guess' => $guess];
-	    return view('roll-dice', $data);
-	}
-});
+Route::get('/rolldice/{guess?}', 'ExampleController@rollDice');
