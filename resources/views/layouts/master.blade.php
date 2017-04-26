5fb44af014a08262cd24889cc3455fab86a56388
Matt Valdez <mattvaldez01@gmail.com>
2017-04-26 09:55:38 -0500

    commented out font-awesome link for now

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,8 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Reddit</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	<link rel="stylesheet" href="css/font-awesome.min.css">
-	<link rel="stylesheet" type="text/css" href="">
+<!-- 	<link rel="stylesheet" href="css/font-awesome.min.css">
+ -->	<link rel="stylesheet" type="text/css" href="">
 </head>
 <body>
     @yield('content')
