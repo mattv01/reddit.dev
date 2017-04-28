58250fdab8ac0b379389ff91f655c052c372931a
Matt Valdez <mattvaldez01@gmail.com>
2017-04-28 11:11:49 -0500

    this page shows a form where users can create a post

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.master')
 
 @section('content')
-
+<h1>Creating Post</h1>
 <form  method="POST" action="{{ action('PostsController@store') }}">
 	@include('partials.posts_form')
 	{{ method_field('POST') }}
+	<input type="submit" value="Create Post" class="btn btn-default btn-success">
 </form>
 
 @stop
