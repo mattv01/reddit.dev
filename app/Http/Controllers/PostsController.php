a2461cefd83a042d9191a76c7e0498d4370e2b54
Matt Valdez <mattvaldez01@gmail.com>
2017-04-28 10:58:21 -0500

    wrote out each function to return useful results instead of having them return strings

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -1,87 +1,101 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 
 class PostsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function index(Request $request)
     {
-        return "A listing of all posts";
+        $posts = Post::paginate(4);
+        $data = [];
+        $data['posts'] = $posts;
+        return view('posts.index')->with($data);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+
+    public function create(Request $request)
     {
-        return "Creating new post"; 
+        return view('posts.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(Request $request)
     {
-        return "Updating database with post created";
+        $rules = Post::$rules;
+        $this->validate($request, $rules);
+
+        $post = new Post();
+        $post->title = $request->title;
+        $post->url = $request->url;
+        $post->content = $request->content;
+        $post->created_by = 1;
+        $post->save();
+
+        $request->session()->flash('successMessage', 'Post saved successfully');
+        return redirect()->action('PostsController@show', [$post->id]);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+
+    public function show(Request $request, $id)
     {
-        return "Showing Post #";
+        $post = Post::find($id);
+        if (!$post) {
+            $request->session()->flash('errorMessage', 'Post cannot be found');
+            return redirect()->action('PostsController@index');
+        }
+        $data = [];
+        $data['post'] = $post;
+        return view('posts.show')->with($data);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
+
+    public function edit(Request $request, $id)
     {
-        return "Editing post";
+        $post = Post::find($id);
+        if (!$post) {
+            $request->session()->flash('errorMessage', 'Post cannot be found');
+            return redirect()->action('PostsController@index');
+        }
+        $data = [];
+        $data['post'] = $post;
+        return view('posts.edit')->with($data);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function update(Request $request, $id)
     {
-        return "Updated post";
+        $rules = Post::$rules;
+        $this->validate($request, $rules);
+        
+        $post = Post::find($id);
+        if (!$post) {
+            $request->session()->flash('errorMessage', 'Post cannot be found');
+            return redirect()->action('PostsController@index');
+        }
+        $post->title = $request->title;
+        $post->url = $request->url;
+        $post->content = $request->content;
+        $post->created_by = 1;
+        $post->save();
+
+        $request->session()->flash('successMessage', 'Post updated successfully');
+        return redirect()->action('PostsController@show', [$post->id]);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
+
+    public function destroy(Request $request, $id)
     {
-        return "Deleting post";
+        $post = Post::find($id);
+        if (!$post) {
+            $request->session()->flash('errorMessage', 'Post cannot be found');
+            return redirect()->action('PostsController@index');
+        }
+        $post->delete();
+
+        $request->session()->flash('successMessage', 'Post deleted successfully');
+        return redirect()->action('PostsController@index');
     }
 }
