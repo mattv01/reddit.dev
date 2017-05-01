f3f981259baafa54760f7766f89bcda04fcf37e4
Matt Valdez <mattvaldez01@gmail.com>
2017-05-01 10:23:12 -0500

    added routes for login/logout and register

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -30,3 +30,13 @@ Route::get('/rolldice/{guess?}', 'ExampleController@rollDice');
 
 
 Route::resource('posts', 'PostsController');
+
+
+// Authentication routes...
+Route::get('/login', 'Auth\AuthController@getLogin');
+Route::post('/login', 'Auth\AuthController@postLogin');
+Route::get('/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('/register', 'Auth\AuthController@getRegister');
+Route::post('/register', 'Auth\AuthController@postRegister');
