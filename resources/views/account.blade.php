fb61d3fb29b7a0792aa57e900b29a12ee56990e0
Matt Valdez <mattvaldez01@gmail.com>
2017-05-03 13:43:49 -0500

    wrote out account page

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -1,7 +1,32 @@
 @extends('layouts.master')
 
 @section('content')
-<h1>Your Account</h1>
 
+<section class="text-center">
+	<h1>Your Account</h1>
+	<p style="font-size: 1.5em"><strong>Full Name:</strong> {{Auth::user()->name}}</p>
+	<p style="font-size: 1.5em"><strong>Email:</strong> {{Auth::user()->email}}</p>
+</section>
+
+<section>
+	<h1 class="text-center" style="margin-top: 5%">Your Posts</h1>
+	@foreach ($posts as $post)
+		@if (Auth::id() == $post->created_by)
+		    <article class="col-md-4" style="height: 15em">
+		        <h3>{{ $post->id }}: <a href="{{ action('PostsController@show', $post->id) }}">{{ $post->title }}</a></h3>
+		        <p>{{ $post->url }}</p>
+		    	@if (strlen($post->content)>125)
+            	    <p>{{ substr($post->content, 0, 125). "... " }}<a href="{{ action('PostsController@show', $post->id) }}">Full Description</a>	</p>
+            	@else
+            	    <p>{{$post->content}}</p>
+            	@endif
+		        <p>Written on: {{ $post->created_at->toDayDateTimeString() }}</p>            
+		        @if($post->created_at != $post->updated_at)
+		            <p>Edited {{ $post->updated_at->diffForHumans() }}</p>
+		        @endif
+		    </article>
+		@endif
+	@endforeach
+</section>
 
 @stop
